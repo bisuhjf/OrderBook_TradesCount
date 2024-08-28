ee102a7ca8cfe62d95dca3572c7f292b051b6900
bisuhjf <89909044+bisuhjf@users.noreply.github.com>
2024-08-28 17:09:28 +0800

    Update okx_orderbook_rest.py

--- okx_orderbook_rest.py
+++ okx_orderbook_rest.py
@@ -7,10 +7,10 @@ import numpy as np
 import pandas as pd
 
 
-okx_symbol = 'BTC-USDT'
+ok_symbol = 'BTC-USDT'
 
 
-def okx_ob(symbol):
+def ok_ob(symbol):
     url = "https://www.okx.com/api/v5/market/books"
     params = {'instId': symbol, 'sz': 10}
     response = requests.get(url, params=params)
@@ -28,11 +28,8 @@ def okx_ob(symbol):
         asks_df['diff_volume'] = asks_df['diff'] * asks_df['volume']
         bids_sum = sum(bids_df['diff_volume'])
         asks_sum = sum(asks_df['diff_volume'])
-        return bids_sum, asks_sum
     else:
         print('Error Code: {}'.format(response.status_code))
         bids_sum, asks_sum = 0, 0
     return bids_sum, asks_sum
 
-
-print(okx_ob(okx_symbol))
