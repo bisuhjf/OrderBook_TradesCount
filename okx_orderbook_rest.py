4c6f0fa5c2b10c28c4b7cf4707fd8bdfd587d4fd
bisuhjf <89909044+bisuhjf@users.noreply.github.com>
2024-08-28 16:33:38 +0800

    Add files via upload

---
+++ okx_orderbook_rest.py
@@ -0,0 +1,38 @@
+import base64
+import hmac
+import json
+import time
+import requests
+import numpy as np
+import pandas as pd
+
+
+okx_symbol = 'BTC-USDT'
+
+
+def okx_ob(symbol):
+    url = "https://www.okx.com/api/v5/market/books"
+    params = {'instId': symbol, 'sz': 10}
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        ob = response.json()
+        data = ob['data']
+        asks = data[0]['asks']
+        bids = data[0]['bids']
+        asks_df = pd.DataFrame(asks, columns=['price', 'volume', 'num1', 'num2'])[0:10].astype(float)
+        bids_df = pd.DataFrame(bids, columns=['price', 'volume', 'num1', 'num2'])[0:10].astype(float)
+        mid = (asks_df['price'].iloc[0] + bids_df['price'].iloc[0]) / 2
+        bids_df['diff'] = mid - bids_df['price']
+        asks_df['diff'] = asks_df['price'] - mid
+        bids_df['diff_volume'] = bids_df['diff'] * bids_df['volume']
+        asks_df['diff_volume'] = asks_df['diff'] * asks_df['volume']
+        bids_sum = sum(bids_df['diff_volume'])
+        asks_sum = sum(asks_df['diff_volume'])
+        return bids_sum, asks_sum
+    else:
+        print('Error Code: {}'.format(response.status_code))
+        bids_sum, asks_sum = 0, 0
+    return bids_sum, asks_sum
+
+
+print(okx_ob(okx_symbol))
